c28db26599500cc737bfea946d87371d0d69786e
Srikumar K. S <srikumarks@gmail.com>
2017-02-26 07:00:40 +0530

    We now add a little bit more convenience to this scheme of things. If we now have a function that is called to dynamically return a child, then it would be useful to also be able to dynamically return an array of children to be inserted at the appropriate point.

--- src/oak.js
+++ src/oak.js
@@ -69,70 +69,103 @@ now do both of these at one shot. The main offshoot of this change is
 that the children can be computed *after* the parent, which wasn't
 the case with the plain functional model.
 
+tag('div', { classList: { button: true, large: true },
+             model: { urgent: true },
+      function (model) {
+        return model.urgent ? "Click me now!" : "Click me!";
+      });
+
+## v6
+
+We now add a little bit more convenience to this scheme of things.
+If we now have a function that is called to dynamically return a child,
+then it would be useful to also be able to dynamically return an array
+of children to be inserted at the appropriate point.
+
 */
 var tag = function tag(name, attrs) {
-    var e = document.createElement(name);
-    
-    if (attrs) {
-        for (var k in attrs) {
-            var valOrFn = attrs[k];
 
-            if (typeof valOrFn === 'function') {
-                switch (k) {
-                    case 'model':
-                        // This is a special attribute that we use to attach a
-                        // model object to the node.
-                        e.model = valOrFn(e.model);
-                        break;
-                    case 'style':
-                        processStyle(valOrFn(e.model), e);
-                        break;
-                    case 'classList':
-                        processClassList(valOrFn(e.model), e);
-                        break;
-                    default:
-                        // If an attribute key is of the form onclick/onmouseup/etc.
-                        // then we treat the value as a handler to be attached 
-                        // to the element. Note that within a handler of the form
-                        // function (event) {...}, the model object can be accessed
-                        // using `event.target.model`.
-                        if (/^on/.test(k)) {
-                            e.addEventListener(k.substring(2), valOrFn);
-                        } else {
-                            // Invoke the given function to determine the value
-                            // of the attribute.
-                            var val = valOrFn(e.model, k);
-                            if (val) {
-                                e.setAttribute(k, val.toString());
+    function $oak$render(model) {
+        var e = document.createElement(name);
+        e.model = model
+        e.dyno = []; // Holds the list of things to be dynamically updated in-place.
+
+        // Store away the element as a render function property
+        // so that it can be modified in-place.
+        $oak$render.element = e;
+
+        if (attrs) {
+            for (var k in attrs) {
+                var valOrFn = attrs[k];
+
+                if (typeof valOrFn === 'function') {
+                    switch (k) {
+                        case 'model':
+                            // This is a special attribute that we use to attach a
+                            // model object to the node.
+                            e.model = valOrFn(e.model);
+
+                            // This is supposed to be dynamic, so we set things up
+                            // using the 'dyno' mechanism so that this property will
+                            // be updated whenever the model changes.
+                            e.dyno.push(dynmodel(valOrFn));
+                            break;
+                        case 'style':
+                            processStyle(valOrFn(e.model), e);
+                            break;
+                        case 'classList':
+                            processClassList(valOrFn(e.model), e);
+                            break;
+                        default:
+                            // If an attribute key is of the form onclick/onmouseup/etc.
+                            // then we treat the value as a handler to be attached 
+                            // to the element. Note that within a handler of the form
+                            // function (event) {...}, the model object can be accessed
+                            // using `event.target.model`.
+                            if (/^on/.test(k)) {
+                                e.addEventListener(k.substring(2), valOrFn);
+                            } else {
+                                // Invoke the given function to determine the value
+                                // of the attribute.
+                                var val = valOrFn(e.model, k);
+                                if (val) {
+                                    e.setAttribute(k, val.toString());
+                                }
+
+                                // This is a dynamic attribute, so we need to set it
+                                // so that it will be updated when the model changes.
+                                e.dyno.push(dynattr(valOrFn, k));
                             }
-                        }
-                }
-            } else if (valOrFn) {
-                // Normal value to be set.
-                switch (k) {
-                    case 'model':
-                        e.model = valOrFn;
-                        break;
-                    case 'style':
-                        processStyle(valOrFn, e);
-                        break;
-                    case 'classList':
-                        processClassList(valOrFn, e);
-                        break;
-                    default:
-                        // This is surely not a handler, since the
-                        // value supplied is not a function.
-                        e.setAttribute(k, valOrFn.toString());
+                    }
+                } else if (valOrFn) {
+                    // Normal value to be set.
+                    switch (k) {
+                        case 'model':
+                            e.model = valOrFn;
+                            break;
+                        case 'style':
+                            processStyle(valOrFn, e);
+                            break;
+                        case 'classList':
+                            processClassList(valOrFn, e);
+                            break;
+                        default:
+                            // This is surely not a handler, since the
+                            // value supplied is not a function.
+                            e.setAttribute(k, valOrFn.toString());
+                    }
                 }
             }
         }
+
+        for (var i = 2; i < arguments.length; ++i) {
+            installContents(contents, e);
+        }
+
+        return e;
     }
-    
-    for (var i = 2; i < arguments.length; ++i) {
-        installContents(arguments[i], e);
-    }
-    
-    return e;
+
+    return $oak$render;
 };
 
 var processStyle = function processStyle(style, e) {
@@ -157,13 +190,57 @@ var installContents = function installContents(contents, e) {
         var child = contents(e.model);
         if (typeof child === 'string') {
             child = document.createTextNode(child);
+        } else if (child instanceof Array) {
+            for (var i = 0; i < child.length; ++i) {
+                installContents(child[i], e);
+            }
+        } else {
+            e.appendChild(child);
         }
-        e.appendChild(child);
     } else if (contentsType === 'string') {
         e.appendChild(document.createTextNode(contents));
+    } else if (contents instanceof Array) {
+        // We support array type so it is easy to splice lists of
+        // DOM nodes when rendering.
+        for (var i = 0; i < contents.length; ++i) {
+            installContents(contents[i], e);
+        }
     } else if (contents) {
         e.appendChild(contents);
     }
 };
+
+function dynattr(fn, k) {
+    return function (e) {
+        var val = fn(e.model, k);
+        if (val) {
+            e.setAttribute(k, val.toString());
+        } else {
+            e.removeAttribute(k);
+        }
+    };
+}
+
+function dynstyle(fn, attr) {
+    return function (e) {
+        e.style[attr] = fn(e.model);
+    };
+}
+
+function dynclasslist(fn, attr) {
+    return function (e) {
+        if (fn(e.model)) {
+            e.classList.add(attr);
+        } else {
+            e.classList.remove(attr);
+        }
+    };
+}
+
+function dynmodel(fn) {
+    return function (e) {
+        e.model = fn(e.model);
+    };
+}
 
 mod.tag = tag;
